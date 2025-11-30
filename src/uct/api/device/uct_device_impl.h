25941b6b9e421d4b0f0170b9c65c34d027fd3499
Hod Badihi <hbadihi@login-eos01.eos.clusters.nvidia.com>
2025-11-30 05:59:26 -0800

    UCP: Add put_with_imm API

--- src/uct/api/device/uct_device_impl.h
+++ src/uct/api/device/uct_device_impl.h
@@ -71,6 +71,52 @@ UCS_F_DEVICE ucs_status_t uct_device_ep_put_single(
 
 /**
  * @ingroup UCT_DEVICE
+ * @brief Posts one memory put operation with immediate data.
+ *
+ * This device routine writes a single memory block from the local address @a address
+ * to the remote address @a remote_address using the device endpoint @a device_ep,
+ * along with immediate data @a imm_data.
+ * The memory element @a mem_elem must be valid and contain the local and remote
+ * memory regions to be transferred.
+ *
+ * User can pass @a comp to track execution and completion status.
+ * The @a flags parameter can be used to modify the behavior
+ * of the routine.
+ *
+ * @param [in]  device_ep       Device endpoint to be used for the operation.
+ * @param [in]  mem_elem        Memory element representing the memory to be transferred.
+ * @param [in]  address         Local virtual address to send data from.
+ * @param [in]  remote_address  Remote virtual address to write data to.
+ * @param [in]  length          Length in bytes of the data to send.
+ * @param [in]  imm_data        Immediate data to send.
+ * @param [in]  flags           Flags to modify the function behavior.
+ * @param [in]  comp            Completion object to track the progress of operation.
+ *
+ * @return UCS_INPROGRESS     - Operation successfully posted, use @ref
+ *                              uct_device_ep_progress and @ref
+ *                              uct_device_ep_check_completion to
+ *                              check for completion.
+ * @return UCS_OK             - Operation completed successfully.
+ * @return Error code as defined by @ref ucs_status_t
+ */
+template<ucs_device_level_t level>
+UCS_F_DEVICE ucs_status_t uct_device_ep_put_single_with_imm(
+        uct_device_ep_h device_ep, const uct_device_mem_element_t *mem_elem,
+        const void *address, uint64_t remote_address, size_t length,
+        uint64_t imm_data, uint64_t flags, uct_device_completion_t *comp)
+{
+    if (device_ep->uct_tl_id == UCT_DEVICE_TL_RC_MLX5_GDA) {
+        return uct_rc_mlx5_gda_ep_put_with_imm<level>(device_ep, mem_elem,
+                                                      imm_data, remote_address,
+                                                      flags, comp, address);
+    }
+
+    return UCS_ERR_UNSUPPORTED;
+}
+
+
+/**
+ * @ingroup UCT_DEVICE
  * @brief Posts one atomic add operation.
  *
  * This device routine increments a single memory value by @a inc_value using the
