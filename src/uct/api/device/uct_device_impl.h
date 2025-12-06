63c61c4e4bef937313a079259c4556d84bac152d
Hod Badihi <hbadihi@login-eos01.eos.clusters.nvidia.com>
2025-12-06 12:09:36 -0800

    fix compilation bugs

--- src/uct/api/device/uct_device_impl.h
+++ src/uct/api/device/uct_device_impl.h
@@ -399,11 +399,11 @@ UCS_F_DEVICE ucs_status_t uct_device_ep_check_completion(
          uint64_t imm_data, uint64_t remote_address, uint64_t flags,
          uct_device_completion_t *comp, const void *address)
  {
-     if (device_ep->uct_tl_id == UCT_DEVICE_TL_RC_MLX5_GDA) {
-         return uct_rc_mlx5_gda_ep_put_with_imm<level>(device_ep, mem_elem,
-                                                     imm_data, remote_address,
-                                                     flags, comp, address);
-     }
+    if (device_ep->uct_tl_id == UCT_DEVICE_TL_RC_MLX5_GDA) {
+        return uct_rc_mlx5_gda_ep_put_with_imm<level>(device_ep, mem_elem,
+                                                    imm_data, remote_address,
+                                                    flags, comp);
+    }
      return UCS_ERR_UNSUPPORTED;
  }
 
